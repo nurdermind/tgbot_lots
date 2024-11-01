ab139f20c047e80aacda307181e4270029d6c510
Ranil Sadykov <87027444+Shy11er@users.noreply.github.com>
2024-11-01 16:28:26 +0500

    feat

--- models/lot.py
+++ models/lot.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, Index
+from sqlalchemy import Column, Integer, String, Index
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
--- parsers/catalog_lot_online_parser.py
+++ parsers/catalog_lot_online_parser.py
@@ -1,9 +1,7 @@
-import time
 from selenium import webdriver
 from selenium_stealth import stealth
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import re
 from logger_config import logger
